fdbc0d1a8cfb8b2257ab071680ee75279ff5f530
Chris Elion <chris.elion@unity3d.com>
2020-04-02 20:53:27 -0700

    first pass

---
+++ IDMProject/Assets/Scripts/WaypointNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointNode : MonoBehaviour
+{
+    public List<WaypointNode> Edges = new List<WaypointNode>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
