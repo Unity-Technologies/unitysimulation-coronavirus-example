7cf5a0c2725ee9faacf09ca11534bd0b34ee5e09
Chris Elion <chris.elion@unity3d.com>
2020-04-07 22:19:05 -0700

    Better waypoint config (#8)
    * Cleanup edge connection and drawing code
    
    * add angle theshold to waypoint

--- IDMProject/Assets/Scripts/StoreSimulation.cs
+++ IDMProject/Assets/Scripts/StoreSimulation.cs
@@ -150,76 +150,47 @@ public class StoreSimulation : MonoBehaviour
             }
         }
 
-        var directions = new Vector3[]
-        {
-            Vector3.forward,
-            Vector3.back,
-            Vector3.left,
-            Vector3.right,
-        };
-
         var startTicks = DateTime.Now.Ticks;
-        if (false)
+        // TODO avoid duplication with the gizmos code by passing a delegate?
+        foreach (var wp in waypoints)
         {
-            // Raycast all possible pairs of points
-            for (var i = 0; i < waypoints.Length; i++)
+            foreach (var otherWaypoint in waypoints)
             {
-
-                for (var j = i + 1; j < waypoints.Length; j++)
+                if (wp == otherWaypoint)
                 {
-                    var wp1 = waypoints[i];
-                    var wp2 = waypoints[j];
-                    var rayStart = wp1.transform.position;
-                    var rayEnd = wp2.transform.position;
-                    RaycastHit hitInfo;
-                    var didHit = Physics.Raycast(rayStart, rayEnd - rayStart, out hitInfo);
-                    if (didHit && hitInfo.collider?.gameObject == wp2.gameObject)
-                    {
-                        // Don't add incoming edges to Entrances, or outgoing edges from Exits.
-                        if (!wp2.IsEntrance() && !wp1.IsExit())
-                        {
-                            wp1.Edges.Add(wp2);
-                        }
-                        if(!wp1.IsEntrance() && !wp2.IsExit())
-                        {
-                            wp2.Edges.Add(wp1);
-                        }
-                    }
+                    continue;
+                }
+
+                var validConnection = wp.ShouldConnect(OneWayAisles, otherWaypoint);
+                if (validConnection)
+                {
+                    wp.Edges.Add(otherWaypoint);
                 }
             }
         }
-        else
-        {
-            for (var i = 0; i < waypoints.Length; i++)
-            {
-                var wp = waypoints[i];
-                if (wp.IsEntrance())
-                {
-                    // For entrances, check all other points
-                    for (var j = 0; j < waypoints.Length; j++)
-                    {
-                        if (i == j)
-                        {
-                            continue;
-                        }
-
-                        var dir = (waypoints[j].transform.position - wp.transform.position).normalized;
-                        wp.CheckRaycastConnection(OneWayAisles, dir);
-                    }
-                }
-                else
-                {
-                    // For other waypoints, just check preset directions
-                    foreach (var dir in directions)
-                    {
-                        wp.CheckRaycastConnection(OneWayAisles, dir);
-                    }
-                }
-            }
-        }
-
         var endTicks = DateTime.Now.Ticks;
         Debug.Log($"Raycasting between waypoints took {(endTicks-startTicks)*s_TicksToSeconds} seconds");
+    }
+
+    void OnDrawGizmos()
+    {
+        var tempWaypoints = GetComponentsInChildren<WaypointNode>();
+        foreach (var wp in tempWaypoints)
+        {
+            foreach (var otherWaypoint in tempWaypoints)
+            {
+                if (wp == otherWaypoint)
+                {
+                    continue;
+                }
+
+                var validConnection = wp.ShouldConnect(OneWayAisles, otherWaypoint);
+                if (validConnection)
+                {
+                    wp.DrawEdge(otherWaypoint);
+                }
+            }
+        }
     }
 
     void UpdateExposure()
