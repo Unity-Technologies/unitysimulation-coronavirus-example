82fb404d662a7b1da5bb4f82c96f77899d86bd5b
Rambod Kermanizadeh <rambod@MacBook-Pro-9.attlocal.net>
2020-04-10 22:59:22 -0700

    Added text mesh pro, and GUI to scene for interactivity

---
+++ IDMProject/Assets/Scripts/StoreSimGui.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StoreSimGui : MonoBehaviour
+{
+    public StoreSimulation storeSimulation;
+
+    public Slider numShoppersSlider;
+    public Slider numContagiousSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numShoppersSlider.value = storeSimulation.DesiredNumShoppers;
+        numContagiousSlider.maxValue = numShoppersSlider.value;
+        numContagiousSlider.value = storeSimulation.DesiredNumContagious;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetSim()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnNumShoppersChanged()
+    {
+        storeSimulation.DesiredNumShoppers = (int)numShoppersSlider.value;
+        numContagiousSlider.maxValue = storeSimulation.DesiredNumShoppers;
+    }
+
+    public void OnNumContagiousChanged()
+    {
+        storeSimulation.DesiredNumContagious = (int)numContagiousSlider.value;
+    }
+}
